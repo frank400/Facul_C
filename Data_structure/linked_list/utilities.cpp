3efd1e7eea630c44ee0d91ee7beb8188ccbc4dbb
frank400 <j.victorhuguenin2018@gmail.com>
2020-02-23 12:46:24 -0300

    new function to delete a node

--- Data_structure/linked_list/utilities.cpp
+++ Data_structure/linked_list/utilities.cpp
@@ -35,3 +35,44 @@ void push(Node** head_ref,int n){
     new_node->next=(*head_ref);
     (*head_ref)=new_node;
 }
+
+void insertAfter(Node* prev_node, int new_data)  
+{  
+    /*1. check if the given prev_node is NULL */
+    if (prev_node == NULL)  
+    {  
+        cout<<"the given previous node cannot be NULL";  
+        return;  
+    }  
+  
+    /* 2. allocate new node */
+    Node* new_node = new Node(); 
+  
+    /* 3. put in the data */
+    new_node->data = new_data;  
+  
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;  
+  
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node;  
+} 
+
+void delete_node(Node** head_ref,int key){
+    Node* temp = *head_ref , *prev;
+    
+    if (temp != NULL && temp->data==key){
+        *head_ref=temp->next;
+        free(temp);
+        return;
+    }
+    if (temp!=NULL && temp->data!= key){
+        prev=temp;
+        temp=temp->next;
+        delete_node(&temp,key);
+    }
+    if (temp==NULL){return;}
+    
+    prev->next=temp->next;
+    free(temp);
+}
--- Data_structure/linked_list/utilities.h
+++ Data_structure/linked_list/utilities.h
@@ -4,4 +4,6 @@ void sort_linked_list();
 void show_linked_list(Node* n);
 void append(Node** head_ref, int n);
 void push(Node** head_ref,int n);
+void insertAfter(Node* prev_node, int new_data);
+void delete_node(Node** head_ref,int key);
 #endif
