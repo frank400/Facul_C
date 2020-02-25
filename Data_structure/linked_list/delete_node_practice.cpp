06429de1f2c04c3a7d508d6a86bf23ea35743dc4
frank400 <j.victorhuguenin2018@gmail.com>
2020-02-24 23:54:48 -0300

    comment the code to more easily understandment

--- Data_structure/linked_list/delete_node_practice.cpp
+++ Data_structure/linked_list/delete_node_practice.cpp
@@ -7,6 +7,7 @@
 int main(){
     
     //T is the number of tests cases that are going to be executed
+    std::cout<<"enter the number of test do you want: ";
     int T;
     std::cin>>T;
     for(int cout=1;cout<=T;cout++){
@@ -16,23 +17,28 @@ int main(){
         std::cout<<"enter the value for N: ";
         std::cin>> N;
 
+        //create a string to store the numbers that the user inputs
         std::string numbers;
         
-
+        //clear the buffer of cin stream
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        
-        std::cout<<"enter the N numbers that you want to insert on the list: \n";
+        std::cout<<"enter N numbers that you want to insert on the list: \n";
         std::getline(std::cin,numbers);
 
+        //use a stream to manipulate the numbers gived
         std::stringstream num_stream(numbers);
+        //store the number itself so it can be converted to int type and send to the append function
         std::string number;
+
+        //loops all the numbers inserting each one of the on the end of the linked list
         while(std::getline(num_stream,number,' ')){
             int value=atoi(number.c_str());
             
             append(&head,value);
             
         }
+        std::cout<<"enter the position that you want to delete starting from 0 :";
         int x;
         cin>>x;
         delete_position(&head,x);
